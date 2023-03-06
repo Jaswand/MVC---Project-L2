b3d3f1754c61ecd2ecc29077c60ec75a1d8ea265
Jaswand Soekhoe <jp.soekhoe@outlook.com>
2023-03-06 14:30:20 +0100

    Added MVC

--- mvc-framework-io-sd-2109A/app/libraries/Core.php
+++ mvc-framework-io-sd-2109A/app/libraries/Core.php
@@ -3,13 +3,38 @@
 class Core
 {
     // Properties
+    public $currentController = 'Landingpages';
+    public $currentMethod = 'index';
+    public $params = [];
 
     // Constructor
     public function __construct()
     {
         $url = $this->getURL();
         
-        var_dump($url);exit();
+        if (file_exists('../app/controllers/'. ucwords($url[0]) . '.php')) {
+            $this->currentController = ucwords($url[0]);
+            unset($url[0]);
+        }
+
+        // require_once het bestand waar de controllerclass in zit
+        require_once '../app/controllers/'. $this->currentController . '.php';
+
+        // Maak een nieuw object van de controllerclass
+        $this->currentController = new $this->currentController();
+     
+        if (isset($url[1])) {
+            if (method_exists($this->currentController, $url[1])) {
+                $this->currentMethod = $url[1];
+                unset($url[1]);
+            }
+        }
+
+        // ternary operator
+        // Dit is de parameterlijst van je method in de controllerclass
+        $this->params = $url ? array_values($url): [];
+
+        call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
     }  
     
     public function getURL()
@@ -22,6 +47,8 @@ class Core
             $url = explode('/', $url);
 
             return $url;
+        } else {
+            return array('Landingpages', 'index');
         }
     }
 }
