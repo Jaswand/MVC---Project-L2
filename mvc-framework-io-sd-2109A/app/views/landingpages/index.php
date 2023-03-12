e4b21466cea423ac09b4e297a47bdb1a974833eb
Jaswand Soekhoe <jp.soekhoe@outlook.com>
2023-03-12 19:13:37 +0100

    Added text

--- mvc-framework-io-sd-2109A/app/views/landingpages/index.php
+++ mvc-framework-io-sd-2109A/app/views/landingpages/index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
     <link rel="stylesheet" href="css/Homepage.css">
+    <link rel="stylesheet" href="css/Buttons.css">
     <title>Rocambolesque</title>
 </head>
 <body>
@@ -13,11 +14,14 @@
    <div class="bg-image">
    <img src="/public/img/white.png" alt="white" style="width: 25%">
     <div class="centered">
-        <h1>Alleen Kwaliteit</h1>
+        <h1 class="text-kwaliteit">Alleen Kwaliteit</h1>
         <a class="btn" href="#">Bekijk ons menu</a>
         <a class="btn" href="#">Reserveer nu</a>
   </div>
 </div>
+
+<p class="text-registreren">U Kunt hier ook een account aanmaken</p>
+<a class="btn-r" href="#">Registreren</a>
     
  
  
