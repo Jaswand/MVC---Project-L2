80dddf6540c45aa2c4be9ff689a450c49d3b95a2
Jaswand Soekhoe <jp.soekhoe@outlook.com>
2023-03-12 16:20:37 +0100

    Added buttons en text

---
+++ mvc-framework-io-sd-2109A/app/views/includes/footer.php
@@ -0,0 +1,33 @@
+<Footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="footer-col">
+                <h4>ABOUT US</h4>
+                <ul>
+                    <li><a href="#">Privacy verklaring</a></li>
+                    <li><a href="#">Algemene voorwaarden</a></li>
+                    <li><a href="#">Betaling</a></li>  
+                    <li><a href="#">FAQ</a></li>
+                    <li><a href="#">Onze verzend informatie </a></li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h4>UW ACCOUNT</h4>
+                <ul>
+                    <li><a href="#">Persoonlijke info</a></li>
+                    <li><a href="#">Bestellingen</a></li>
+                    <li><a href="#">Creditnota's</a></li>
+                    <li><a href="#">Mijn notificaties</a></li>
+                    <li><a href="#">Waardebonnen  </a></li>
+                </ul>
+        </div>
+            <div class="footer-col">
+                <h4>FOLLOW US ON OUR SOCIAL MEDIA  </h4>
+                <div class="social-links">
+                    <a href="#"><i class="ion-social-facebook"></i></a>
+                    <a href="#"><i class="ion-social-instagram"></i></a>
+                    <a href="#"><i class="ion-social-snapchat"></i></a>
+                    <a href="#"><i class="ion-social-discord"></i></a>
+            </div>
+        </div>
+    </Footer>
