76146402ea50b641a074e2ea4b598accfd9bef21
Glitchula <65823161+Glitchula@users.noreply.github.com>
2023-03-14 14:17:26 +0100

    Navbar Finished
    Navbar

--- mvc-framework-io-sd-2109A/app/views/landingpages/index.php
+++ mvc-framework-io-sd-2109A/app/views/landingpages/index.php
@@ -1,10 +1,29 @@
-<h3><?= $data['title']; ?></h3>
-<p><?= $data['sayHello']; ?></p>
-<ul>
-    <li>
-        <a href="<?= URLROOT; ?>/countries/index">show countries</a>        
-    </li>
-    <li>
-        <a href="<?= URLROOT; ?>/lessen/index">Lesrooster weergeven</a>
-    </li>
-</ul>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/navbar.css">
+    <title>Navbar</title>
+</head>
+
+<body>
+    <nav>
+        <div class="logo">
+            <img src="img/Rocambolesque-logo-DEF.png" alt="Logo">
+        </div>
+        <ul class="nav-links vertical-line">
+            <li class="horizontal-line"><a href="#">Home</a></li>
+            <li class="horizontal-line"><a href="#">About</a></li>
+            <li class="horizontal-line"><a href="#">Contact</a></li>
+        </ul>
+        <ul class="nav-links">
+            <li class="horizontal-login-line"><a href="#">Login</a></li>
+        </ul>
+
+    </nav>
+</body>
+
+</html>
