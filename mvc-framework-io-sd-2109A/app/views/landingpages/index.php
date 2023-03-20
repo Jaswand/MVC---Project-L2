766f28969bc01b93bddd87c63086aeecdfff3222
Glitchula <65823161+Glitchula@users.noreply.github.com>
2023-03-20 13:54:56 +0100

    Navbar finished

--- mvc-framework-io-sd-2109A/app/views/landingpages/index.php
+++ mvc-framework-io-sd-2109A/app/views/landingpages/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,26 +8,31 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
     <link rel="stylesheet" href="css/Homepage.css">
     <link rel="stylesheet" href="css/Buttons.css">
+    <link rel="stylesheet" href="css/navbar.css">
     <title>Rocambolesque</title>
 </head>
-<body>
-<div class="container">
-   <div class="bg-image">
-   <img src="/public/img/white.png" alt="white" style="width: 25%">
-    <div class="centered">
-        <h1 class="text-kwaliteit">Alleen Kwaliteit</h1>
-        <a class="btn" href="#">Bekijk ons menu</a>
-        <a class="btn" href="#">Reserveer nu</a>
-  </div>
-</div>
 
-<p class="text-registreren">U Kunt hier ook een account aanmaken</p>
-<a class="btn-r" href="#">Registreren</a>
-    
- 
- 
-<?php
-require_once '../app/views/includes/footer.php';
-?>
-    
-  
+<body>
+
+    <?php
+    require_once '../app/views/includes/navbar.php';
+    ?>
+
+    <div class="container">
+        <div class="bg-image">
+            <img class="white-rectangle" src="/public/img/white.png" alt="white" style="width: 25%">
+            <div class="centered">
+                <h1 class="text-kwaliteit">Alleen Kwaliteit</h1>
+                <a class="btn" href="#">Bekijk ons menu</a>
+                <a class="btn" href="#">Reserveer nu</a>
+            </div>
+        </div>
+
+        <p class="text-registreren">U Kunt hier ook een account aanmaken</p>
+        <a class="btn-r" href="#">Registreren</a>
+
+
+
+        <?php
+        require_once '../app/views/includes/footer.php';
+        ?>
