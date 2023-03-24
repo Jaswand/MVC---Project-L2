1a81728b581641e83f44463b3df970bf24748d69
Jaswand Soekhoe <jp.soekhoe@outlook.com>
2023-03-24 13:43:37 +0100

    Savepoint navbar

---
+++ mvc-framework-io-sd-2109A/app/views/includes/Navbar.php
@@ -0,0 +1,13 @@
+<nav>
+    <div class="logo">
+        <img src="img/Rocambolesque-logo-DEF.png" alt="Logo">
+    </div>
+    <ul class="nav-links vertical-line">
+        <li class="horizontal-line"><a href="#">Home</a></li>
+        <li class="horizontal-line"><a href="#">About</a></li>
+        <li class="horizontal-line"><a href="#">Contact</a></li>
+    </ul>
+    <ul class="nav-right">
+        <li class="horizontal-login-line"><a href="#">Login</a></li>
+    </ul>
+</nav>
--- mvc-framework-io-sd-2109A/app/views/landingpages/index.php
+++ mvc-framework-io-sd-2109A/app/views/landingpages/index.php
@@ -11,12 +11,13 @@ require_once '../app/views/includes/Navbar.php';
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
     <link rel="stylesheet" href="css/Homepage.css">
     <link rel="stylesheet" href="css/Buttons.css">
+    <link rel="stylesheet" href="css/navbar.css">
     <title>Rocambolesque</title>
 </head>
 <body>
 <div class="container">
    <div class="bg-image">
-   <img src="/public/img/white.png" alt="white" style="width: 25%">
+   <img class="white-rectangle" src="/public/img/white.png" alt="white" style="width: 25%">
     <div class="centered">
         <h1 class="text-kwaliteit">Alleen Kwaliteit</h1>
         <a class="btn" href="#">Bekijk ons menu</a>
